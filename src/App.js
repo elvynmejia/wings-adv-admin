ff8cec5dfaa6c831e01de770cd7fc3c9cb5e3f6c
elvynmejia <elvynmejia@gmail.com>
2020-04-15 18:30:53 -0700

    Adds login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import restProvider from 'ra-data-simple-rest';
+import LoginPage from './pages/login';
 
 const API_ROOT = "https://wings-adv.herokuapp.com"; //https://wings-adv.herokuapp.com
 const EMAIL = "admin@wings.com";
@@ -13,7 +14,11 @@ const HEADERS = {
 };
 
 const App = () => (
-  <Admin dataProvider={restProvider(API_ROOT)}>
+  <Admin 
+    dataProvider={restProvider(API_ROOT)}
+    loginPage={LoginPage}
+    authProvider={authProvider}
+  >
     {/*
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     */}
@@ -21,3 +26,13 @@ const App = () => (
 );
 
 export default App;
+
+
+const authProvider = {
+    login: params => Promise.resolve(),
+    logout: params => Promise.resolve(),
+    checkAuth: params => Promise.resolve(),
+    checkError: error => Promise.resolve(),
+    getPermissions: params => Promise.resolve(),
+};
+
