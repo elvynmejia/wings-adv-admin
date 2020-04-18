ae7296bd4ca5cecff1be855a58e4490b6a02473d
elvynmejia <elvynmejia@gmail.com>
2020-04-18 13:05:02 -0700

    Adds date parse/formatted util

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+import moment from "moment";
+import { isNumber } from "lodash";
+
+const DATE_FORMAT = "MMMM Do, YYYY";
+
+const parseAndFormatDate = (value = null, format = DATE_FORMAT) => {
+  if (value === null) {
+    return value;
+  }
+
+  let date = "";
+
+  if (isNumber(value)) {
+    date = moment.unix(value).utc();
+  } else {
+    date = moment(value).utc();
+  }
+
+  if (format) {
+    return date.format(format);
+  }
+
+  return date;
+};
+
+export default parseAndFormatDate;
