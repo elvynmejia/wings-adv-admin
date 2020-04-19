5d5c6be98e6d8f9da27423f1c35df91c8b06a2ff
elvynmejia <elvynmejia@gmail.com>
2020-04-19 15:08:21 -0700

    Uses useUpdate hook

--- src/pages/user/verify.js
+++ src/pages/user/verify.js
@@ -2,62 +2,59 @@ import React, { Fragment, useState } from "react";
 import {
   Button,
   Confirm,
-  useUpdateMany,
-  useRefresh,
+  useUpdate,
   useNotify,
-  useUnselectAll,
+  Notification,
 } from "react-admin";
 
-import CheckIcon from '@material-ui/icons/Check';
+import CheckIcon from "@material-ui/icons/Check";
 
 const VerifyButton = (props) => {
-  console.log(`props => ${JSON.stringify(props)}`);
+  // apiAction is a hack to be able to PUT/PATCH to users/id/verify for example
+  const apiAction = 'verify';
+  const { record, source } = props;
   const [open, setOpen] = useState(false);
-  const refresh = useRefresh();
   const notify = useNotify();
-  const unselectAll = useUnselectAll();
-  // const [updateMany, { loading }] = useUpdateMany(
-  //   "posts",
-  //   selectedIds,
-  //   { views: 0 },
-  //   {
-  //     onSuccess: () => {
-  //       refresh();
-  //       notify("Posts updated");
-  //       unselectAll("posts");
-  //     },
-  //     onFailure: (error) => notify("Error: posts not updated", "warning"),
-  //   }
-  // );
-
-  const loading = false;
-  const updateMany = () => {
-    console.log("................. doing something .............");
-  };
 
   const handleClick = () => setOpen(true);
   const handleDialogClose = () => setOpen(false);
 
-  const handleConfirm = () => {
-    updateMany();
-    setOpen(false);
-  };
-
-  const { record, source } = props;
+  const [confirm, { loading }] = useUpdate(
+    "users",
+    record.id,
+    { apiAction },
+    record,
+    {
+      undoable: true,
+      onSuccess: ({ data }) => {
+        setOpen(false);
+        notify("User verified", "info", {}, true);
+      },
+      onFailure: (error) => {
+        setOpen(false);
+        notify(`Error: ${error.message}`, "warning");
+      },
+    }
+  );
 
   if (record[source]) {
     return null;
   }
-  
+
   return (
     <Fragment>
-      <Button label="Verify user" onClick={handleClick} startIcon={<CheckIcon />} />
+      <Button
+        label="Verify user"
+        onClick={handleClick}
+        startIcon={<CheckIcon />}
+      />
+      <Notification />
       <Confirm
         isOpen={open}
         loading={loading}
         title="Verify New User"
         content="Are you sure you want to verify this new user?"
-        onConfirm={handleConfirm}
+        onConfirm={confirm}
         onClose={handleDialogClose}
       />
     </Fragment>
