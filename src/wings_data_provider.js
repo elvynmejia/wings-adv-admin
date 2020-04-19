f1cc045e7d2bd400dcd6c242c289c75560adcfd5
elvynmejia <elvynmejia@gmail.com>
2020-04-19 15:07:24 -0700

    Patches dataProvider#update to be able to send a request to a non-standard CRUD endpoint

--- src/wings_data_provider.js
+++ src/wings_data_provider.js
@@ -1,2 +1,34 @@
 import restProvider from "ra-data-simple-rest";
-export default restProvider;
+const provider = (apiUrl, httpClient) => {
+  const dataProvider = restProvider(apiUrl, httpClient);
+
+  return {
+    ...dataProvider,
+    // patch https://github.com/marmelab/react-admin/blob/master/packages/ra-data-simple-rest/src/index.ts#L111
+    update: (resource, params) => {
+      // apiAction is a hack to be able to PUT/PATCH to /admin/v1//users/:id/verify for example
+      const EXTRACT_BEFORE_REQUEST = ["apiAction"];
+
+      const { data } = params;
+      const { apiAction } = data;
+      const baseUrl = `${apiUrl}/${resource}/${params.id}`;
+      const url = apiAction ? `${baseUrl}/${apiAction}` : baseUrl;
+
+      const body = Object.keys(data).reduce((obj, key) => {
+        if (!EXTRACT_BEFORE_REQUEST.includes(key)) {
+          return {
+            ...obj,
+            [key]: data[key],
+          };
+        }
+        return {...obj };
+      }, {});
+      return httpClient(url, {
+        method: "PATCH", // should be able to do PUT as well
+        body: JSON.stringify(body),
+      }).then(({ json }) => ({ data: json }));
+    },
+  };
+};
+
+export default provider;
